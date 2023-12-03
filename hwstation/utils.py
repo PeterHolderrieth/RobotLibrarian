5c79f0d2f48cf23220091795a1ce3b6ae41ff8bc
Peter Holderrieth <peterholderrieth@31-38-116.wireless.csail.mit.edu>
2023-12-03 16:16:59 -0500

    Modified mobile iiwa

--- hwstation/utils.py
+++ hwstation/utils.py
@@ -1,36 +1,49 @@
-from IPython.display import HTML, SVG, display
-from pydrake.systems.framework import Diagram
-from manipulation.station import MakeHardwareStation, load_scenario
-from manipulation.utils import ConfigureParser
+import numpy as np
+import trimesh
+import sys
+from typing import List 
+from IPython.display import clear_output, SVG
 from pydrake.all import (
-    RigidTransform,
-    Simulator,
-    MultibodyPlant,
-    DiagramBuilder,
-    Meshcat, 
-    DiagramBuilder, 
-    Parser, 
     AddMultibodyPlantSceneGraph,
+    Concatenate,
+    DiagramBuilder,
     MeshcatVisualizer,
+    MeshcatVisualizerParams,
+    Parser,
+    PointCloud,
+    RigidTransform,
+    StartMeshcat,
+    UniformlyRandomRotationMatrix,
+    Context,
     Diagram,
-    Context
+    PointCloud,
+    Simulator,
+    TrajectorySource,
+    Solve,
+    RotationMatrix,
+    MultibodyPlant
 )
+from pydrake.geometry import Meshcat
+from pydrake.multibody import inverse_kinematics
 
+from manipulation.station import MakeHardwareStation, load_scenario
+from manipulation.meshcat_utils import AddMeshcatTriad
+from manipulation import running_as_notebook
+from manipulation.scenarios import AddFloatingRpyJoint, AddRgbdSensors, ycb, AddMultibodyTriad, MakeManipulationStation
+from manipulation.utils import ConfigureParser
+from manipulation.clutter import GraspCandidateCost, GenerateAntipodalGraspCandidate
+from manipulation.icp import IterativeClosestPoint
 from manipulation.station import AddPointClouds
 
+# Own utils
+from enum import Enum
 import pydot
 
-def setup_builder(meshcat: Meshcat, scenario_data: str):
-
+def init_diagram(meshcat: Meshcat, scenario_data: str):
     meshcat.Delete()
     builder = DiagramBuilder()
-    plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=0.00)
-    
     scenario = load_scenario(data=scenario_data)
-    station = builder.AddSystem(MakeHardwareStation(scenario, meshcat))
-
-    parser = Parser(plant)
-    ConfigureParser(parser)
+    station = builder.AddSystem(MakeHardwareStation(scenario, meshcat,parser_preload_callback=ConfigureParser))
 
     # Adding point cloud extractors:
     to_point_cloud = AddPointClouds(
@@ -40,27 +53,28 @@ def setup_builder(meshcat: Meshcat, scenario_data: str):
     #Connect point clouds with output port:
     for idx, name in enumerate(to_point_cloud.keys()):
         builder.ExportOutput(
-            to_point_cloud[name].get_output_port(), name+"_ptcloud"
-        )
+            to_point_cloud[name].get_output_port(), name+"_ptcloud")
 
-    return builder, plant, scene_graph, station, parser, scenario
+    builder.ExportOutput(
+        station.GetOutputPort("mobile_iiwa.state_estimated"),
+        "mobile_iiwa.state_estimated"
+    )
 
-def plot_and_simulate(meshcat: Meshcat, builder: DiagramBuilder, plant: MultibodyPlant, station: Diagram, time_end: float, context: Context = None):
+    builder.ExportInput(
+        station.GetInputPort("mobile_iiwa.desired_state"),
+        "mobile_iiwa.desired_state"
+    )
 
-    #plant.Finalize()
     visualizer = MeshcatVisualizer.AddToBuilder(builder, station.GetOutputPort("query_object"), meshcat)
     diagram = builder.Build()
     diagram.set_name("plant and scene_graph")
-    simulator = Simulator(station)
+    simulator = Simulator(diagram)
+    return diagram, visualizer, simulator
 
-    #Fix desired state of mobile iiwa to be the initial position
+def fix_input_port(diagram: Diagram, simulator: Simulator):
     sim_context = simulator.get_mutable_context()
-    x0 = station.GetOutputPort("mobile_iiwa.state_estimated").Eval(sim_context)
-    station.GetInputPort("mobile_iiwa.desired_state").FixValue(sim_context, x0)
-
-    simulator.AdvanceTo(time_end)
-    
-    return diagram, simulator, sim_context
+    x0 = diagram.GetOutputPort("mobile_iiwa.state_estimated").Eval(sim_context)
+    diagram.GetInputPort("mobile_iiwa.desired_state").FixValue(sim_context, x0)
 
 def visualize_diagram(diagram: Diagram):
     return SVG(pydot.graph_from_dot_data(diagram.GetGraphvizString())[0].create_svg())
